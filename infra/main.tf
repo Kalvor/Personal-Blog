527ea1fc0c2a165c3b88377c3e0929a3ba4c0c02
Krzysztof <krzysztofjadczak55@gmail.com>
2024-03-05 01:47:41 +0100

    Done

--- infra/main.tf
+++ infra/main.tf
@@ -4,41 +4,41 @@ module "resource_group" {
   name     = var.resource_group_name
 }
 
-# module "vnet" {
-#   depends_on                 = [module.resource_group.id]
-#   source                     = "./modules/vnet"
-#   location                   = var.resource_group_location
-#   rg_name                    = var.resource_group_name
-#   nsg_name                   = var.vnet_nsg_name
-#   vnet_name                  = var.vnet_name
-#   k8s_subnet_name            = var.k8s_subnet_name
-#   k8s_subnet_address         = var.k8s_subnet_address
-#   app_gateway_subnet_address = var.app_gateway_subnet_address
-#   app_gateway_subnet_name    = var.app_gateway_subnet_name
-#   address_space              = var.vnet_address_space
-# }
+module "vnet" {
+  depends_on                 = [module.resource_group.id]
+  source                     = "./modules/vnet"
+  location                   = var.resource_group_location
+  rg_name                    = var.resource_group_name
+  nsg_name                   = var.vnet_nsg_name
+  vnet_name                  = var.vnet_name
+  k8s_subnet_name            = var.k8s_subnet_name
+  k8s_subnet_address         = var.k8s_subnet_address
+  app_gateway_subnet_address = var.app_gateway_subnet_address
+  app_gateway_subnet_name    = var.app_gateway_subnet_name
+  address_space              = var.vnet_address_space
+}
 
-# module "app_gateway" {
-#   depends_on     = [module.resource_group.id]
-#   source         = "./modules/app_gateway"
-#   name           = var.app_gateway_name
-#   location       = var.resource_group_location
-#   rg_name        = var.resource_group_name
-#   subnet_id      = module.vnet.app_gateway_subnet_id
-#   subnet_name    = var.app_gateway_name
-#   public_ip_name = var.app_gateway_public_ip_name
-# }
+module "app_gateway" {
+  depends_on     = [module.resource_group.id]
+  source         = "./modules/app_gateway"
+  name           = var.app_gateway_name
+  location       = var.resource_group_location
+  rg_name        = var.resource_group_name
+  subnet_id      = module.vnet.app_gateway_subnet_id
+  subnet_name    = var.app_gateway_name
+  public_ip_name = var.app_gateway_public_ip_name
+}
 
-# module "aks" {
-#   depends_on            = [module.resource_group.id]
-#   source                = "./modules/aks"
-#   location              = var.resource_group_location
-#   rg_name               = var.resource_group_name
-#   rg_id                 = module.resource_group.id
-#   acr_name_prefix       = var.acr_name_prefix
-#   aks_name              = var.aks_name
-#   aks_dns_prefix        = var.aks_dns_prefix
-#   subnet_id             = module.vnet.k8s_subnet_id
-#   app_gateway_id        = module.app_gateway.id
-#   app_gateway_subnet_id = module.vnet.app_gateway_subnet_id
-# }
+module "aks" {
+  depends_on            = [module.resource_group.id]
+  source                = "./modules/aks"
+  location              = var.resource_group_location
+  rg_name               = var.resource_group_name
+  rg_id                 = module.resource_group.id
+  acr_name_prefix       = var.acr_name_prefix
+  aks_name              = var.aks_name
+  aks_dns_prefix        = var.aks_dns_prefix
+  subnet_id             = module.vnet.k8s_subnet_id
+  app_gateway_id        = module.app_gateway.id
+  app_gateway_subnet_id = module.vnet.app_gateway_subnet_id
+}
--- infra/vars.tfvars
+++ infra/vars.tfvars
@@ -1,5 +1,5 @@
 resource_group_location = "North Europe"
-resource_group_name     = "RG-Personal-BlogTest"
+resource_group_name     = "RG-Personal-Blog"
 
 vnet_address_space         = "10.10.0.0/16"
 k8s_subnet_name            = "personal-blog-k8s"
