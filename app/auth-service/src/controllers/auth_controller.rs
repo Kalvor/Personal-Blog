7824c3c2de3d0dd1a34b3029463b79958029a2b8
Krzysztof <krzysztofjadczak55@gmail.com>
2024-03-08 00:50:01 +0100

    Auth Controller scatch, postman integration

--- app/auth-service/src/controllers/auth_controller.rs
+++ app/auth-service/src/controllers/auth_controller.rs
@@ -1,7 +1,7 @@
 use axum::response::IntoResponse;
 use axum::Json;
 use axum::Router;
-use axum::routing::{get,post};
+use axum::routing::post;
 use hyper::StatusCode;
 use log::info;
 
@@ -9,7 +9,7 @@ use crate::contracts::request::LoginRequest;
 
 pub fn router() -> Router{
     Router::new()
-        .route("/api/v1/service/login", post(login))
+        .route("/api/v1/auth/login", post(login))
 }
 
 pub async fn login(Json(login_request): Json<LoginRequest>) -> impl IntoResponse {
