3386c6188b43b226b8ee8ab7fa5291e44e408ada
Krzysztof <krzysztofjadczak55@gmail.com>
2024-03-09 13:07:35 +0100

    Sql Terraform fixed

--- infra/modules/sql/main.tf
+++ infra/modules/sql/main.tf
@@ -1,33 +1,41 @@
-resource "azurerm_mssql_server" "server" {
+resource "azurerm_mysql_server" "server" {
   name                         = var.name
   resource_group_name          = var.rg_name
   location                     = var.location
-  version                      = "12.0"
-  administrator_login          = "personalblog_admin"
+  version                      = "8.0"
+  administrator_login          = "sqladmin"
   administrator_login_password = var.admin_password
+  sku_name   = "B_Gen5_1"
+  storage_mb = 5120
+  auto_grow_enabled                 = false
+  backup_retention_days             = 7
+  geo_redundant_backup_enabled      = false
+  infrastructure_encryption_enabled = false
+  public_network_access_enabled     = true
+  ssl_enforcement_enabled           = true
+  ssl_minimal_tls_version_enforced  = "TLS1_2"
+}
+
+resource "time_sleep" "wait_2mins" {
+  depends_on = [azurerm_mysql_server.server]
+
+  create_duration = "120s"
 }
 
 resource "azurerm_mysql_database" "auth-service-db" {
-  depends_on = [azurerm_mssql_server.server]
+  depends_on = [time_sleep.wait_2mins]
   name                = var.authdb_name
   resource_group_name = var.rg_name
-  server_name         = azurerm_mssql_server.server.name
+  server_name         = azurerm_mysql_server.server.name
   charset             = "utf8"
   collation           = "utf8_unicode_ci"
-
-  lifecycle {
-    prevent_destroy = true
-  }
 }
 
 resource "azurerm_mysql_database" "blog-service-db" {
-  depends_on = [azurerm_mssql_server.server]
+  depends_on = [time_sleep.wait_2mins]
   name                 = var.blogdb_name
   resource_group_name  = var.rg_name
-  server_name          = azurerm_mssql_server.server.name
+  server_name          = azurerm_mysql_server.server.name
   charset             = "utf8"
   collation           = "utf8_unicode_ci"
-  lifecycle {
-    prevent_destroy = true
-  }
 }
